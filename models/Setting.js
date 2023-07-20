e11ea910868ad50fc872879d1d13ce87a2f5659d
HASSAN AHMED <hassaansultan720@gmail.com>
2023-07-20 11:30:10 +0500

    Ecommerce-Admin is completed

---
+++ models/Setting.js
@@ -0,0 +1,11 @@
+import { model, models, Schema } from "mongoose";
+
+const settingSchema = new Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    value: { type: Object },
+  },
+  { timestamps: true }
+);
+
+export const Setting = models?.Setting || model("Setting", settingSchema);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#5542F6",
+        highlight: "#eae8fb",
+        bgGray: "#fbfafd",
       },
     },
   },
   plugins: [],
-}
+};
